f251176891783e4da165127fbf53ac6ba0a595a8
Dhanurasiya.M <dhanurashya@gmail.com>
2026-07-26 17:01:04 +0530

    Fix formatting of base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/AI_Career_Intelligence/'
+  base: '/AI_Career_Intelligence/',
 
   server: {
     port: 3000,
